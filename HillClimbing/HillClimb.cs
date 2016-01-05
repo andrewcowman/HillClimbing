29d850020fc3bf0550d1a9442a5979e3184a9fa3
andrewcowman <acowman2@gmail.com>
2016-01-04 23:10:00 -0600

    HC

---
+++ HillClimbing/HillClimb.cs
@@ -0,0 +1,79 @@
+﻿namespace HillClimbing {
+    /// <summary>
+    /// Hill climbing training algorithm.
+    /// </summary>
+    public class HillClimb {
+
+        private Network _net; // network to perform hill climbing on
+        private Scorer _scorer; // scoring object
+        private double[] _stepSize; // size of the step for each value in _data
+        private double[] _candidate = new double[5]; // potential moves for the algorithm to take
+
+        private double _lastError; // last error
+
+        public double LastError {
+            get { return _lastError; }
+        }
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="net">Network to perform hill climbing algorithm on.</param>
+        /// <param name="score">Scoring object.</param>
+        /// <param name="accel">Acceleration of step sizes.</param>
+        /// <param name="stepSize">Initial step sizes.</param>
+        public HillClimb(Network net, Scorer score, double accel, double stepSize) {
+            _net = net;
+            _scorer = score;
+            _stepSize = new double[net.LongTermMemory.Length];
+
+            for (int i = 0; i < _stepSize.Length; i++) {
+                _stepSize[i] = stepSize;
+            }
+
+            _candidate[0] = -accel;
+            _candidate[1] = -(1/accel);
+            _candidate[2] = 0;
+            _candidate[3] = 1/accel;
+            _candidate[4] = accel;
+
+            _lastError = double.MaxValue;
+        }
+
+        /// <summary>
+        /// Run through an iteration of hill climbing.
+        /// </summary>
+        public void Iteration() {
+            
+            // loop over all dimensions and try to improve each
+            for(int i = 0; i < _net.LongTermMemory.Length; i++) {
+
+                int best = -1; // index of best candidate
+                double bestScore = double.MaxValue; // score to minimize
+
+                // try each candidate move for this dimension
+                for(int j = 0; j < _candidate.Length; j++) {
+                    
+                    // try the candidate and score it, then back out of the candidate
+                    _net.LongTermMemory[i] += _stepSize[i] * _candidate[j];
+                    double temp = _scorer.Score(_net);
+                    _net.LongTermMemory[i] -= _stepSize[i] * _candidate[j]; // back out
+
+                    // keep track of candidate with most improvement
+                    if(temp < bestScore) {
+                        bestScore = temp;
+                        best = j;
+                        _lastError = bestScore;
+                    }
+                }
+
+                // if there was a best candidate
+                // move in that direction
+                if(best != -1) {
+                    _net.LongTermMemory[i] += _stepSize[i] * _candidate[best];
+                    _stepSize[i] = _stepSize[i] * _candidate[best];
+                }
+            }
+        }
+    }
+}
---
+++ HillClimbing/Program.cs
@@ -0,0 +1,109 @@
+﻿using System.Reflection;
+using System.IO;
+using System.Collections.Generic;
+using System;
+
+namespace HillClimbing {
+    class Program {
+
+        static void Main(string[] args) {
+            Program prg = new Program();
+            prg.Run();
+        }
+
+        public void Run() {
+
+             // get iris file from resource stream
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            var f = assembly.GetManifestResourceStream("HillClimbing.Resources.iris.csv");
+            StreamReader sr = new StreamReader(f);
+
+            // load all records into new dataset
+            DataSet ds = DataSet.Load(sr);
+            sr.Close();
+
+            Dictionary<string, int> species = ds.EncodeOneOfN(4); // encode specie names using one of n encoding
+
+            // get a list of records with data and encoded class name
+            List<Record> trainingData = ds.ExtractSupervised(0, 4, 4, 3);
+
+            Network net = new Network(4, 4, 2); // new RBF network
+            net.Randomize(); // initialize the networks state
+            Scorer score = new Scorer(trainingData); // new scorer
+            HillClimb hc = new HillClimb(net, score, 1.2, 1); // new hill climbing training algorithm
+            Iterate(hc, 100, 0.01); // iterate through hill climbing algorithm
+            QueryOneOfN(net, trainingData, species); // display ideal and actual specie names
+        }
+
+        /// <summary>
+        /// Iterate through a hill climbing algorithm.
+        /// Tries to minimize score.
+        /// </summary>
+        /// <param name="hc">Algorithm to iterate through.</param>
+        /// <param name="numIterations">Maximum iterations.</param>
+        /// <param name="minScore">Minimum score.</param>
+        private void Iterate(HillClimb hc, int numIterations, double minScore) {
+            int iterationNumber = 0; // number of iterations
+            bool done = false; // is the algorithm done?
+
+            // while not done...
+            do {
+                iterationNumber++; // increase number of iterations
+                hc.Iteration(); // run an iteration
+
+                // if iteration reaches limit or score reaches min score
+                if(iterationNumber >= numIterations || hc.LastError < minScore) {
+                    done = true; // set done = true
+                }
+
+                // write out iteration # and score
+                Console.WriteLine("Iteration #" + iterationNumber + ", Score=" + hc.LastError + " (Minimize)");
+            } while(!done);
+        }
+
+        /// <summary>
+        /// Goes through each record in the training set and displays the ideal output and the actual output.
+        /// </summary>
+        /// <param name="net">RBF network.</param>
+        /// <param name="trainingData">Training data.</param>
+        /// <param name="specieNames">Specie names.</param>
+        private void QueryOneOfN(Network net, List<Record> trainingData, Dictionary<string, int> species) {
+            // invert the specie list
+            Dictionary<int, string> invSpecies = new Dictionary<int, string>();
+            foreach(string key in species.Keys) {
+                int value = species[key];
+                invSpecies[value] = key;
+            }
+
+            // for each training record
+            foreach(Record rec in trainingData) {
+
+                double[] output = net.ComputeRegression(rec.Input); // run the input through the RBF network
+
+                int idealIdx = GetMax(rec.Ideals); // find the index of the ideal name
+                int actualIdx = GetMax(output); // find the index of the actual name
+
+                Console.WriteLine("Guess: " + invSpecies[actualIdx] + " Actual: " + invSpecies[idealIdx]);
+            }
+        }
+
+        /// <summary>
+        /// Returns the index with the max value.
+        /// </summary>
+        /// <param name="da">Array to go through.</param>
+        /// <returns>Index of the max value.</returns>
+        private int GetMax(double[] da) {
+            int result = -1;
+            double max = double.MinValue;
+
+            for(int i = 0; i < da.Length; i++) {
+                if(da[i] > max) {
+                    max = da[i];
+                    result = i;
+                }
+            }
+
+            return result;
+        }
+    }
+}
